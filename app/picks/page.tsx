91d9831b3b67392d7ef184d93b61a939647813f1
howimetyourmower-cmd <howimetyourmower@gmail.com>
2025-11-11 15:38:22 +1100

    Update page.tsx

--- app/picks/page.tsx
+++ app/picks/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = false
+
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { db } from "@/lib/firebaseClient";
