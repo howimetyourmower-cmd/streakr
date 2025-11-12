d689fd7bc3ec7bc9f3fdad299c187a738ac0280a
howimetyourmower-cmd <howimetyourmower@gmail.com>
2025-11-13 00:03:50 +1100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               <Image
                 src="/streakrlogo.jpg"
                 alt="STREAKr"
-                width={46}
-                height={46}
+                width={66}
+                height={66}
                 priority
                 className="rounded"
               />
