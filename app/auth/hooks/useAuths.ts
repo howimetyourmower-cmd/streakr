53b0e2af09bae52c0f15b98c5fea81268e3ad727
howimetyourmower-cmd <howimetyourmower@gmail.com>
2025-11-15 15:12:05 +1100

    Create useAuths.ts

---
+++ app/auth/hooks/useAuths.ts
@@ -0,0 +1,21 @@
+// hooks/useAuth.ts
+"use client";
+
+import { useEffect, useState } from "react";
+import { auth } from "@/lib/firebaseClient";
+import { onAuthStateChanged, User } from "firebase/auth";
+
+export function useAuth() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (u) => {
+      setUser(u);
+      setLoading(false);
+    });
+    return () => unsub();
+  }, []);
+
+  return { user, loading };
+}
