bcdd4fb3f7a6f39c4cd594aabeb6e33704cfae2e
howimetyourmower-cmd <howimetyourmower@gmail.com>
2025-11-20 07:45:41 +1100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import Link from "next/link";
-import MobileNav from "@/components/MobileNav";
+import MobileNav from "app/components/MobileNav";
 
 export const metadata: Metadata = {
   title: "STREAKr",
