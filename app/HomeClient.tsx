e8205e22bfe8312138ea11c719dc0d47ec9a1d0c
howimetyourmower-cmd <howimetyourmower@gmail.com>
2025-12-20 22:26:08 +1100

    Create HomeClient.tsx

---
+++ app/HomeClient.tsx
@@ -0,0 +1,10 @@
+import { Suspense } from "react";
+import HomeClient from "./HomeClient";
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div className="p-6">Loading…</div>}>
+      <HomeClient />
+    </Suspense>
+  );
+}
