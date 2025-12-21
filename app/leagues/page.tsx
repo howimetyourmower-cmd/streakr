3f60897c91944318558aabd04f800a6528d8c89e
howimetyourmower-cmd <howimetyourmower@gmail.com>
2025-12-21 22:38:22 +1100

    Update page.tsx

--- app/leagues/page.tsx
+++ app/leagues/page.tsx
@@ -1,5 +1,7 @@
-export const dynamic = "force-dynamic"
+
 "use client";
+
+export const dynamic = "force-dynamic"
 
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
