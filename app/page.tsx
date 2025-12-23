c3934c3b3665371f37214c3b21a80661e05553f2
howimetyourmower-cmd <howimetyourmower@gmail.com>
2025-12-23 20:50:30 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 // /app/page.tsx
-import HomeClient from "@/HomeClient"; // if you put it in /src/HomeClient.tsx
-// OR: import HomeClient from "@/components/HomeClient";
+import HomeClient from "./HomeClient";
 
 export default function Page() {
   return <HomeClient />;
