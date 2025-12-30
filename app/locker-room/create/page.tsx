be6b767bd827fd1f2a0d7e64d3692a3a871c728b
howimetyourmower-cmd <howimetyourmower@gmail.com>
2025-12-30 22:47:03 +1100

    Create page.tsx

---
+++ app/locker-room/create/page.tsx
@@ -0,0 +1,8 @@
+// /app/locker-room/create/page.tsx
+export const dynamic = "force-dynamic";
+
+import CreateRoomClient from "./CreateRoomClient";
+
+export default function Page() {
+  return <CreateRoomClient />;
+}
