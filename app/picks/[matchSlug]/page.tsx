db6dbf8e8a88646c0add85887eaac865fff86791
howimetyourmower-cmd <howimetyourmower@gmail.com>
2026-01-01 17:47:08 +1100

    Update page.tsx

--- app/picks/[matchSlug]/page.tsx
+++ app/picks/[matchSlug]/page.tsx
@@ -1,6 +1,5 @@
+// /app/picks/[matchSlug]/page.tsx
 import MatchPicksClient from "./MatchPicksClient";
-
-export const dynamic = "force-dynamic";
 
 export default function Page({ params }: { params: { matchSlug: string } }) {
   return <MatchPicksClient matchSlug={params.matchSlug} />;
