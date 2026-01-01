493e0a235d5107370a643220c4fb94344ab1e979
howimetyourmower-cmd <howimetyourmower@gmail.com>
2026-01-01 18:43:54 +1100

    Update NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -93,7 +93,7 @@ export default function Navbar() {
               alt="Torpie"
               fill
               priority
-              className="object-contain object-left origin-left scale-[1.30]"
+              className="object-contain object-left origin-left scale-[1.25]"
               sizes="(max-width: 768px) 60vw, 520px"
             />
           </div>
