652f2f0072e867556c17960153ee6094461b833a
howimetyourmower-cmd <howimetyourmower@gmail.com>
2026-01-06 20:18:12 +1100

    Delete app/picks/[gameId]/page.tsx

--- app/picks/[gameId]/page.tsx
+++
@@ -1,6 +0,0 @@
-// /app/picks/[gameId]/page.tsx
-import MatchPicksClient from "./MatchPicksClient";
-
-export default function Page({ params }: { params: { gameId: string } }) {
-  return <MatchPicksClient gameId={params.gameId} />;
-}
