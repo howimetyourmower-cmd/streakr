c48d4b72466fecbf1eeb1d2eb3a28d461f558e81
howimetyourmower-cmd <howimetyourmower@gmail.com>
2026-01-11 12:57:48 +1100

    Update NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -16,7 +16,7 @@ export default function NavBar() {
       { href: "/", label: "Home" },
       { href: "/picks", label: "Picks" },
       { href: "/leaderboards", label: "Leaderboards" },
-      { href: "/locker-rooms", label: "Locker Rooms" },
+      { href: "/locker-room", label: "Locker Rooms" },
       { href: "/profile", label: "Profile" },
     ],
     []
