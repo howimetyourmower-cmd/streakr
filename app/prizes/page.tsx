e5deaaa76ccc6b8338cc0cc253be51affab4bbb5
howimetyourmower-cmd <howimetyourmower@gmail.com>
2026-01-16 08:34:50 +1100

    Create page.tsx

---
+++ app/prizes/page.tsx
@@ -0,0 +1,8 @@
+// /app/prizes/page.tsx
+import PrizesClient from "./PrizesClient";
+
+export const dynamic = "force-dynamic";
+
+export default function PrizesPage() {
+  return <PrizesClient />;
+}
