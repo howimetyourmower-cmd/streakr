046af2998f62c1e953756621313ad0a51faafa4f
howimetyourmower-cmd <howimetyourmower@gmail.com>
2026-01-21 20:41:43 +1100

    Create page.tsx

---
+++ app/reversa/picks/page.tsx
@@ -0,0 +1,8 @@
+// /app/reversa/picks/page.tsx
+import PicksClient from "./PicksClient";
+
+export const dynamic = "force-dynamic";
+
+export default function ReversaPicksPage() {
+  return <PicksClient />;
+}
