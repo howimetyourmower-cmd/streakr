e1a3ac101f294a44b0cadbb3b8c12340be9b5f48
howimetyourmower-cmd <howimetyourmower@gmail.com>
2026-03-24 17:51:51 +1100

    Delete app/reversa directory

--- app/reversa/picks/page.tsx
+++
@@ -1,8 +0,0 @@
-// /app/reversa/picks/page.tsx
-import PicksClient from "./PicksClient";
-
-export const dynamic = "force-dynamic";
-
-export default function ReversaPicksPage() {
-  return <PicksClient />;
-}
